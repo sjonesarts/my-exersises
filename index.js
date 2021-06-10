912a6dbf5238f8bf1d7c3c27f56a4ef344e72063
SJonesArts <85701152+sjonesarts@users.noreply.github.com>
2021-06-10 22:34:37 +0000

    Figured out the moon too!

--- index.js
+++ index.js
@@ -2,19 +2,22 @@
 let shuttleName = 'Determination';
 let shuttleSpeedMph = "17500";
 let distancetoMarsKm = 225000000;
+let distanceToMoonKm = 38400;
 const milesPerKm = 0.621
 // 2. Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distancetoMarsKm);
+console.log(typeof distanceToMoonKm)
+console.log(typeof milesPerKm);
+// Code your solution to exercises 3 and 4 here:
 let milesToMars = distancetoMarsKm * milesPerKm;
 let hoursToMars = milesToMars / shuttleSpeedMph
 let daysToMars = hoursToMars / 24
-console.log(shuttleName + " will take" + daysToMars + " days to reach Mars");
-// this is my first shot at this, messed up the spacing!
-// Code your solution to exercises 3 and 4 here:
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars");
 
 
-
-
-
-
-
-// Code your solution to exercise 5 here:
+let milesToMoon = distanceToMoonKm * milesPerKm;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
